d702c1173844f5ce8e3c55decaab66012ada7625
Jacques Pharand <jacquespharand1@gmail.com>
2015-03-03 21:55:14 -0500

    Initial commit
    kickass.lua initial commit. A sort of blackmail to ensure I actually do
    it.

---
+++ kiackass.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua51
+
+--[[ 
+    A wrapper script for kickass.to to help sort through torrents
+
+    Functions::
+        - Search:: Search for keywords on kickass and return a page of results (default:: 1)
+
+]]--
+
+
+local function search()
+    nil
+end
+
+
+local function main()
+    nil
+end
